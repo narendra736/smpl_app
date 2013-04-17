d618572cfd06c5e8275ee3a67825486725c98e08
ch narendra <your.narendra736@gmail.com>
2013-04-17 14:32:21 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmplApp::Application.config.secret_token = '5f1abc9490ab13d2173936d7b2070fa0a140b6a8785e8a2d78f78c83c879b30aebaa9278cc1564a35e4e743860242ff19b7e5eba09aa87966ecdd18ec5f4651f'
